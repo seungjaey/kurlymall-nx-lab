0ffffe9a037cb387453eb085e0f53866926c41b0
seungjae.yuk <seungjae.yuk@kurlycorp.com>
2022-11-07 20:37:58 +0900

    Base data prepared - COLOR BY PAGES

--- nx-extract-color-by-page/index.ts
+++ nx-extract-color-by-page/index.ts
@@ -1,78 +1,57 @@
-import {resolve, dirname} from 'path';
-import {readFile, readdir} from 'fs/promises';
-import {
-  pipe,
-  map,
-  filter,
-  toArray,
-  toAsync,
-  nth,
-  flat,
-  isEmpty,
-  concurrent,
-  each
-} from '@fxts/core'
+import {readFile, writeFile} from 'fs/promises'
+import {dirname, resolve} from 'path'
+import {pipe, map, filter, flat, toAsync, toArray, nth, each, isUndefined, size, range} from '@fxts/core'
+import ALL_PAGE_LIST from './ALL_PAGE_LIST.json'
+import SOURCE_BY_COLOR_DICTIONARY from './SOURCE_BY_COLOR_DICTIONARY.json'
+import ProgressBar from "progress";
+import SVG_FILE_PATH_LIST from "../nx-svg-to-component/SVG_FILE_PATH_LIST.json";
 
 
-type PageItem = string;
+/**
+ page A
+ ㄴ source 1
+ ㄴ source 1-1
+ ㄴ source 1-2
+ ㄴ source 1-1-1
+ ㄴ source 2
+ ㄴ source 2-1
+ ㄴ source 2-2
+ ㄴ source 2-3
+ ...
+ page B
+ ㄴ ...
+ ...
 
-async function checkFileExist(path: string) {
-  try {
-    await readFile(path)
-    return true
-  } catch (error) {
-    return false
-  }
-}
+ -> TOBE
+ [
+ [page A, [source 1, source 1-1, source 1-2, source 1-1-1, source 2, source 2-1, source 2-2, source 2-3 ...]],
+ [page B, [...]
+ ]
+ */
 
-// 디렉토리는 빼고 파일들만 재귀적으로 읽는다.
-async function extractPages(pagesDir: string): Promise<PageItem[]> {
-  const dirContents = await readdir(pagesDir, {
-    withFileTypes: true,
-  });
+const checkExternalModule = (path: string) => !path.startsWith('.')
 
-  const files = await pipe(
-    dirContents,
-    toAsync,
-    filter((content)=> content.isFile()),
-    map(async({name}) => {
-      const absolutePath = `${pagesDir}/${name}`
-      return absolutePath;
-    }),
-    toArray,
-  ) as PageItem[]
+// @ts-ignore
+const checkFileExist = (path: string) => !!SOURCE_BY_COLOR_DICTIONARY[path]
 
-  const dirs = await pipe(
-    dirContents,
-    toAsync,
-    filter((content)=> content.isDirectory()),
-    map(async ({name}) => {
-      const absolutePath = `${pagesDir}/${name}`
-      const fileList = await extractPages(absolutePath)
-      return fileList
-    }),
-    flat,
-    toArray,
-  ) as PageItem[]
+type SimpleKeyValue = Record<string, { depth: number; path: string }>
 
-  return [...files, ...dirs];
-}
+const MEMO: Record<string, SimpleKeyValue> = {};
 
 const IMPORT_REGEX = /import(?:(?:(?:[ \n\t]+([^ *\n\t\{\},]+)[ \n\t]*(?:,|[ \n\t]+))?([ \n\t]*\{(?:[ \n\t]*[^ \n\t"'\{\}]+[ \n\t]*,?)+\})?[ \n\t]*)|[ \n\t]*\*[ \n\t]*as[ \n\t]+([^ \n\t\{\}]+)[ \n\t]+)from[ \n\t]*(?:['"])([^'"\n]+)(['"])/gm;
-const checkNodeModuleByPath = (path: string) => !(path.startsWith('.') || path.startsWith('..'));
 
-type ChildFileTuple = [number, string]
+async function recursiveFindDependencies(path: string, depth: number, rootPath: string) {
+  const nextDepth = depth + 1
+  const buffer = await readFile(path)
+  const baseFileContent = buffer.toString()
+  const baseDirPath = dirname(path)
 
-async function findFileInDepth(aPath: string, depth: number = 0): Promise<[string, ChildFileTuple[]]> {
-  const buffer = await readFile(aPath)
-  const fileContent = buffer.toString()
-  const regexIter = fileContent.matchAll(IMPORT_REGEX)
-  const baseDirPath = dirname(aPath)
+  const importRegexIter = baseFileContent.matchAll(IMPORT_REGEX)
 
-  const arbitraryTargetFileList = pipe(
-    regexIter,
+  const relatedResourceList = pipe(
+    importRegexIter,
     map(iter => nth(4, iter) || ''),
-    filter(targetPath => !checkNodeModuleByPath(targetPath)),
+    filter(targetPath => !checkExternalModule(targetPath)),
     map(targetPath => resolve(baseDirPath, targetPath)),
     map(targetBaseDirPath => [
       `${targetBaseDirPath}.ts`,
@@ -85,110 +64,39 @@ async function findFileInDepth(aPath: string, depth: number = 0): Promise<[strin
       `${targetBaseDirPath}/index.jsx`,
     ]),
     flat,
+    filter(targetPath => checkFileExist(targetPath)),
     toArray,
   )
 
-  const actualFileList = await pipe(
-    arbitraryTargetFileList,
+  // TODO: 방문하지 않은 노드들만 재귀적으로 순회한다.
+  await pipe(
+    relatedResourceList,
     toAsync,
-    map(async targetPath => {
-      const result = await checkFileExist(targetPath)
-      return [targetPath, result]
-    }),
-    filter(args => {
-      const [_, result] = args
-      return result
-    }),
-    map( args => {
-      const [targetPath] = args
-      return targetPath
-    }),
-    concurrent(8),
-    toArray,
-  )
-
-  if (isEmpty(actualFileList)) {
-    return ['end', []]
-  }
-
-  const result = await pipe(
-    actualFileList,
-    toAsync,
-    map(async filePath => {
-      console.log(`${depth}\tfile = ${filePath}`)
-      const childFile = await findFileInDepth(filePath as string, depth + 1)
-      return [[filePath], [childFile]]
-    }),
-    flat,
-    concurrent(10),
-    toArray,
-  ) as ChildFileTuple[]
-
-  return [aPath, result]
-}
-
-/*
-  원하는 데이터
-
-  data = [
-    {
-      [pageFileName]: [
-        // {
-         depth: number,
-         parentFilePath: string
-         childFilePath: string
-        },
-        {
-          depth: number,
-          parentFilePath,
-          childFilePath
-        }
-      ]
-    }
-  ]
-*/
-
-interface InternalDependencyItem {
-  depth: number
-  list: InternalDependencyItem[]
-}
-
-const REF_DATA: Record<string, Partial<InternalDependencyItem>> = {} as const
-
-function recursiveRetrieveInternalDependencies(depth: number = 0, path: string) {
-  // TODO: ReadFile(path)
-  // TODO: Extract dependencies []
-  // TODO: Set dependencies []
-  // TODO: Recursive
-
-}
-
-async function run(workingDir: string) {
-  const totalPageSourcePathList = await extractPages(`${workingDir}/pages`);
-
-  pipe(
-    totalPageSourcePathList,
-    // TODO: 페이지를 순회하며 하위 의존성 배열을 재귀적으로 생성한다.
-    each(path => {
-      recursiveRetrieveInternalDependencies(0, path)
+    filter(targetPath => isUndefined(MEMO[rootPath][targetPath])),
+    each(async targetPath => {
+      MEMO[rootPath][targetPath] = { path: targetPath, depth: nextDepth }
+      await recursiveFindDependencies(targetPath, nextDepth, rootPath)
     })
   )
-
-  /// await writeFile('temp-result.json', JSON.stringify(allPageSourcePathList))
-  /*
-  const result = await pipe(
-    allPageSourcePathList,
-    toAsync,
-    map(async pageSourcePath => {
-      console.log(`pageSourcePath = ${pageSourcePath}`)
-      const targetPaths = await findFileInDepth(pageSourcePath)
-      return [pageSourcePath, targetPaths]
-    }),
-    toArray
-  )
-   */
-  // await writeFile('temp-result.json', JSON.stringify(result))
 }
 
-// run('/Users/hamtori/kurly/kurlymall-nx')
-run('/Users/mk-am16-075/kurly/kurlymall-nx')
+const run = async () => {
+  const pageLength = size(ALL_PAGE_LIST)
+  const progressBar = new ProgressBar('Processing [:bar] :percent :current/:total', {
+    width: 30,
+    total: pageLength
+  })
+  await pipe(
+    range(pageLength),
+    toAsync,
+    each(async cursor => {
+      const pageSourcePath = nth(cursor, ALL_PAGE_LIST) as string
+      MEMO[pageSourcePath] = {};
+      progressBar.tick()
+      await recursiveFindDependencies(pageSourcePath, 0, pageSourcePath)
+    }),
+  )
+  await writeFile('PAGE_BY_DEPENDENCIES.json', JSON.stringify(MEMO))
+}
+
+run()
